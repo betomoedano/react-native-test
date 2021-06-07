b676d8b82b9bf9bb861a6f3c1b6ea7acec839da9
betomoedano <betomoedano@outlook.com>
2021-06-06 21:04:48 -0400

    First commit 🥳

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import tailwind from 'tailwind-rn';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  TouchableOpacity,
+  SafeAreaView, 
+  Button, 
+  Alert } from 'react-native';
 
 export default function App() {
+
+  //const handlePress = () => console.log("text pressed!");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={tailwind("items-center")} >
+      <Text style={tailwind("text-gray-600 text-xl font-medium mt-4")}>Dog's Tinder</Text>
+      <TouchableOpacity onPress={() => alert("image touched! (●'◡'●)")}>
+        <Image 
+          style={{
+            margin: 50,
+            resizeMode:"cover",
+            width: 300,
+            height: 200,
+          }}
+          source={{uri: "https://picsum.photos/id/237/200/300"}}
+        />
+      </TouchableOpacity>
+        <View style={styles.fixToText}>
+          <Button
+            title="No"
+            onPress={() => Alert.alert("Left Button pressed")}
+          />
+          <Button
+            title="Yes"
+            onPress={() => Alert.alert("Rigth Button pressed")}
+          />
+        </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +50,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+  }
 });
